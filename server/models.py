fa7268ff73b8bfd4d2ea3d2c7bedd6c52dc3a001
Ian Scott Martin <ianscottmartin@gmail.com>
2023-10-07 10:01:16 -0600

    cant seed

--- server/models.py
+++ server/models.py
@@ -1,6 +1,75 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.hybrid import hybrid_property
 
-from config import db
+
+from config import db, Bcrypt
+
 
 # Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True)
+    _password_hash = db.Column(db.String)
+
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash  # Fix the getter to return the password hash
+
+    @password_hash.setter
+    def password_hash(self, password):
+        bcrypt_hash = Bcrypt.generate_password_hash(password).decode("utf-8")
+        self._password_hash = bcrypt_hash
+
+    def authenticate(self, password):
+        return Bcrypt.check_password_hash(self._password_hash, password)
+
+    def __repr__(self):
+        return f"User {self.username}, ID: {self.id}"
+
+
+class Artist(db.Model):
+    __tablename__ = "artists"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+
+    def __init__(self, name):
+        self.name = name
+
+
+class Museum(db.Model):
+    __tablename__ = "museums"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    city = db.Column(db.String)
+
+    def __init__(self, name, city):
+        self.name = name
+        self.city = city
+
+
+class Review(db.Model, SerializerMixin):  # Make sure to add SerializerMixin
+    __tablename__ = "reviews"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(
+        db.Integer, db.ForeignKey("users.id"), nullable=False
+    )  # Correct foreign key references
+    museum_id = db.Column(db.Integer, db.ForeignKey("museums.id"))
+    artist_id = db.Column(db.Integer, db.ForeignKey("artists.id"))
+    text = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "museum_id": self.museum_id,
+            "artist_id": self.artist_id,
+            "text": self.text,
+            "rating": self.rating,
+        }
--- server/seed.py
+++ server/seed.py
@@ -9,9 +9,82 @@ from faker import Faker
 # Local imports
 from app import app
 from models import db
+from models import User, Artist, Museum, Review
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     fake = Faker()
     with app.app_context():
         print("Starting seed...")
         # Seed code goes here!
+
+    # Clear existing data
+    # User.query.delete()
+    # Artist.query.delete()
+    # Museum.query.delete()
+    # Review.query.delete()  # Clear existing reviews
+
+    for _ in range(20):
+        username = fake.profile(fields=["username"])["username"]
+        user = User(username=username)
+
+        # user.password_hash = (
+        #     username  # We are calling the password_hash setter method here
+        # )
+        db.session.add(user)
+        db.session.commit()
+
+    artists = [
+        "Vincent van Gogh",
+        "Leonardo da Vinci",
+        "Pablo Picasso",
+        "Claude Monet",
+        "Michelangelo",
+    ]
+
+    for artist_name in artists:
+        artist = Artist(name=artist_name)
+        db.session.add(artist)
+
+    museum_and_cities = [
+        {"name": "Louvre", "city": "Paris"},
+        {"name": "Metropolitan Museum of Art", "city": "New York"},
+        {"name": "National Gallery", "city": "London"},
+        {"name": "Uffizi", "city": "Florence"},
+        {"name": "Rijksmuseum", "city": "Amsterdam"},
+        {"name": "Musee D'Orsay", "city": "Paris"},
+    ]
+    for item in museum_and_cities:
+        museum = Museum(name=item["name"], city=item["city"])
+        db.session.add(museum)
+
+
+def add_reviews(num_reviews=50):
+    for _ in range(num_reviews):
+        user_id = fake.random_int(
+            min=1, max=20
+        )  # Replace with the actual range of user IDs
+        artist_id = fake.random_int(
+            min=1, max=5
+        )  # Replace with the actual range of artist IDs
+        museum_id = fake.random_int(
+            min=1, max=6
+        )  # Replace with the actual range of museum IDs
+        text = fake.paragraph(nb_sentences=3)
+        rating = fake.random_int(min=1, max=5)
+
+        review = Review(
+            user_id=user_id,
+            artist_id=artist_id,
+            museum_id=museum_id,
+            text=text,
+            rating=rating,
+        )
+
+        db.session.add(review)
+
+    add_reviews()  # Add reviews to the database
+
+    db.session.commit()
+
+
+print("Database has been seeded.")
